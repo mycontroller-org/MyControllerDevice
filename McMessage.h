75f2d44eb35e63ac92c6d944ae1a1ede4f78ad6c
Jeeva Kandasamy <jkandasa@gmail.com>
2017-03-24 08:34:10 +0530

    minor fix

--- McMessage.h
+++ McMessage.h
@@ -41,7 +41,9 @@
 #define C_STREAM                      "C_STREAM"          //Used for OTA firmware updates
   
 //Command [C_PRESENTATION] options  
+#define S_AIR_QUALITY                 "S_AIR_QUALITY"     //Air quality
 #define S_ARDUINO_NODE                "S_ARDUINO_NODE"    //Arduino node device
+#define S_BAROMETER                   "S_BARO"            //Barometer
 #define S_BINARY                      "S_BINARY"          //Binary device(on/off).
 #define S_COVER                       "S_COVER"           //Window covers or shades
 #define S_CPU                         "S_CPU"             //Raspberry PI CPU
@@ -54,49 +56,70 @@
 #define S_GAS                         "S_GAS"             //Gas meter
 #define S_GPS                         "S_GPS"             //GPS Sensor
 #define S_HEATER                      "S_HEATER"          //Heater device
-#define S_HUMIDITY                    "S_HUMIDITY"        //Humidity sensor
-#define S_INFORMATION                 "S_INFORMATION"     //LCD text device
+#define S_HUMIDITY                    "S_HUM"             //Humidity sensor
+#define S_INFO                        "S_INFO"            //LCD text device
 #define S_IR                          "S_IR"              //IR sender/receiver device
+#define S_LIGHT_LEVEL                 "S_LIGHT_LEVEL"     //Light level
 #define S_LOCK                        "S_LOCK"            //Lock device
 #define S_MEMORY                      "S_MEMORY"          //Raspberry PI device memory
 #define S_MOTION                      "S_MOTION"          //Motion sensors.
 #define S_MULTIMETER                  "S_MULTIMETER"      //Multimeter device
 #define S_POWER                       "S_POWER"           //Power measuring device, like power meters
+#define S_PWM                         "S_PWM"             //PWM
 #define S_RAIN                        "S_RAIN"            //Rain sensor
-#define S_RGB_LIGHT                   "S_RGB"             //RGB light
+#define S_RGBW_LIGHT                  "S_RGBW_LIGHT"      //RGBW light
+#define S_RGB_LIGHT                   "S_RGB_LIGHT"       //RGB light
 #define S_SMOKE                       "S_SMOKE"           //Smoke sensor.
-#define S_TEMPERATURE                 "S_TEMPERATURE"     //Temperature sensor
+#define S_SOUND                       "S_SOUND"           //Sound
+#define S_SPRINKLER                   "S_SPRINKLER"       //Sprinkler
+#define S_TEMPERATURE                 "S_TEMP"            //Temperature sensor
+#define S_UV                          "S_UV"              //UV
 #define S_WATER                       "S_WATER"           //Water meter, volume
+#define S_WATER_QUALITY               "S_WATER_QUALITY"   //Water quality
 #define S_WEIGHT                      "S_WEIGHT"          //Weight sensor for scales etc.
+#define S_WIND                        "S_WIND"            //Wind
   
 //Command [C_SET] and [C_REQ] options 
 #define V_ARMED                       "V_ARMED"           //Armed status of a security sensor. 1=Armed, 0=Bypassed
 #define V_COUNT                       "V_COUNT"           //Count
 #define V_CURRENT                     "V_CURRENT"         //Current level
 #define V_CUSTOM                      "V_CUSTOM"          //Custom messages used for controller/inter node specific commands, preferably using S_CUSTOM device type.
+#define V_DIRECTION                   "V_DIRECTION"       //Wind direction 0-360 (degrees)
 #define V_DISTANCE                    "V_DISTANCE"        //Distance
 #define V_DOWN                        "V_DOWN"            //Window covering. Down.
+#define V_EC                          "V_EC"              //Water electric conductivity μS/cm (microSiemens/cm)
+#define V_FLOW                        "V_FLOW"            //Flow of water (in meter)
 #define V_FORECAST                    "V_FORCAST"         //Whether forecast. One of "stable", "sunny", "cloudy", "unstable", "thunderstorm" or "unknown"
 #define V_FREE                        "V_FREE"            //Free amount
+#define V_GUST                        "V_GUST"            //Gust
 #define V_HUMIDITY                    "V_HUM"             //Humidity
 #define V_HVAC_FLOW_MODE              "V_HVAC_FLOW_MODE"  //Flow mode for HVAC ("Auto", "ContinuousOn", "PeriodicOn")
 #define V_HVAC_FLOW_STATE             "V_HVAC_FLOW_STATE" //Mode of header. One of "Off", "HeatOn", "CoolOn", or "AutoChangeOver"
+#define V_HVAC_SETPOINT_COOL          "V_HVAC_SETPOINT_COOL" //HVAC cold setpoint
+#define V_HVAC_SETPOINT_HEAT          "V_HVAC_SETPOINT_HEAT" //HVAC/Heater setpoint
 #define V_HVAC_SPEED                  "V_HVAC_SPEED"      //HVAC/Heater fan speed ("Min", "Normal", "Max", "Auto")
 #define V_ID                          "V_ID"              //Optional unique sensor id (e.g. OneWire DS1820b ids)
 #define V_IMPEDANCE                   "V_IMPEDANCE"       //Impedance value
 #define V_IR_RECEIVE                  "V_IR_RECIVE"       //This message contains a received IR-command
+#define V_IR_RECORD                   "V_IR_RECORD"       //Record IR codes S_IR for playback
 #define V_IR_SEND                     "V_IR_SEND"         //Send out an IR-command
 #define V_KWH                         "V_KWH"             //Accumulated number of KWH for a power meter
 #define V_LEVEL                       "V_LEVEL"           //Used for sending level-value
+#define V_LIGHT_LEVEL                 "V_LIGHT_LEVEL"     //Uncalibrated light level. 0-100%. Use V_LEVEL for light level in lux.
+#define V_LOCK_STATUS                 "V_LOCK_STATUS"     //Set or get lock status. 1=Locked, 0=Unlocked
+#define V_ORP                         "V_ORP"             //Water ORP : redox potential in mV
 #define V_PERCENTAGE                  "V_PERCENTAGE"      //Percentage value. 0-100 (%)
+#define V_PH                          "V_PH"              //Water PH
 #define V_POSITION                    "V_POSITION"        //GPS position and altitude. Payload: latitude;longitude;altitude(m). E.g. "55.722526;13.017972;18"
+#define V_POWER_FACTOR                "V_POWER_FACTOR"    //Ratio of real power to apparent power: floating point value in the range [-1,..,1]
 #define V_PRESSURE                    "V_PRESSURE"        //Atmospheric Pressure or any other
-#define V_RATE                        "V_RAIN"            //Amount of rain
-#define V_RATE                        "V_RATE"            //Rate of rain, other rate
+#define V_RAIN                        "V_RAIN"            //Amount of rain
+#define V_RAINRATE                    "V_RAINRATE"        //Rate of rain
+#define V_RATE                        "V_RATE"            //other rate
 #define V_RGB                         "V_RGB"             //RGB value transmitted as ASCII hex string (I.e "#ff0000" for red)
 #define V_RGBW                        "V_RGBW"            //RGBW value transmitted as ASCII hex string (I.e "#ff0000ff" for red + full white)
-#define V_SETPOINT_COOL               "V_SETPOINT_COOL"   //Cold setpoint
-#define V_SETPOINT_HEAT               "V_SETPOINT_HEAT"   //Heater setpoint
+#define V_SCENE_OFF                   "V_SCENE_OFF"       //Turn OFF a scene
+#define V_SCENE_ON                    "V_SCENE_ON"        //Turn ON a scene
 #define V_STATUS                      "V_STATUS"          //Binary status. 0=off 1=on
 #define V_STOP                        "V_STOP"            //Window covering. Stop.
 #define V_TEMPERATURE                 "V_TEMP"            //Temperature
@@ -105,17 +128,19 @@
 #define V_TRIPPED                     "V_TRIPPED"         //Tripped status of a security sensor. 1=Tripped, 0=Untripped
 #define V_UP                          "V_UP"              //Window covering. Up.
 #define V_USED                        "V_USED"            //Used amount
+#define V_UV                          "V_UV"              //UV light level
 #define V_VA                          "V_VA"              //Apparent power: volt-ampere (VA)
+#define V_VAR                         "V_VAR"             //Reactive power: volt-ampere reactive (var)
 #define V_VAR1                        "V_VAR1"            //Custom value
 #define V_VAR2                        "V_VAR2"            //Custom value
 #define V_VAR3                        "V_VAR3"            //Custom value
 #define V_VAR4                        "V_VAR4"            //Custom value
 #define V_VAR5                        "V_VAR5"            //Custom value
-#define V_VAR                         "V_VAR"             //Reactive power: volt-ampere reactive (var)
 #define V_VOLTAGE                     "V_VOLTAGE"         //Voltage level
 #define V_VOLUME                      "V_VOLUME"          //Water volume
 #define V_WATT                        "V_WATT"            //Watt value for power meters
 #define V_WEIGHT                      "V_WEIGHT"          //Weight (for scales etc)
+#define V_WIND                        "V_WIND"            //Windspeed
 
 //Command [C_INTERNAL] options
 #define I_BATTERY_LEVEL               "I_BATTERY_LEVEL"       //Use this to report the battery level (in percent 0-100).
